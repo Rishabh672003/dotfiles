6b8527889953bc8da607a67d970e2eb03f0707bc
Rishabh <jharishabh2k3@gmail.com>
2022-10-27 20:45:38 +0530

    update

---
+++ .config/yadm/yadm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yadm add ~/.gitignore;
+yadm add ~/.config/alacritty
+yadm add ~/.config/btop;
+yadm add ~/.config/fish;
+yadm add ~/.config/helix;
+yadm add ~/.config/hypr;
+yadm add ~/.config/kitty;
+yadm add ~/.config/mpv;
+yadm add ~/.config/neofetch;
+yadm add ~/.config/nvim;
+yadm add ~/.config/shellfetch;
+yadm add ~/.config/sway;
+yadm add ~/.config/tmux;
+yadm add ~/.config/vim;
+yadm add ~/.config/waybar;
+yadm add ~/.config/wezterm;
+yadm add ~/.config/wofi;
+yadm add ~/.config/zsh;
+yadm add ~/.config/yadm;
+yadm commit -m "update";
+yadm push
