f1e0ee06415da1ad5f1c088a32bc65583a144e7a
Rishabh <jharishabh2k3@gmail.com>
2022-10-28 18:23:50 +0530

    update

--- other-configs/mirrors-and-hosts/rate-mirrors-arch.sh
+++ other-configs/mirrors-and-hosts/rate-mirrors-arch.sh
@@ -7,5 +7,5 @@ sudo true; \
     && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
     && sudo mv "$TMPFILE" /etc/pacman.d/mirrorlist \
     && sudo chmod 777 /etc/pacman.d/mirrorlist  \
-    && sudo bash /home/rishabh/projects/hosts/hosts-maker.sh
-    # && sudo journalctl --vacuum-time=2d \
+    && sudo bash /home/rishabh/projects/hosts/hosts-maker.sh \
+    && sudo journalctl --vacuum-time=2d \
