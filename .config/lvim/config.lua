0cbbed74a5945e1afecec269974729c492c2f8ab
Rishabh <jharishabh2k3@gmail.com>
2022-11-08 20:13:32 +0530

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -6,15 +6,6 @@ require("user.cybu")
 require("user.zen-mode")
 require("user.illuminate")
 require("user.lsp")
-require("lab").setup({
-	code_runner = {
-		enabled = true,
-	},
-	quick_data = {
-		enabled = true,
-	},
-})
-
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ command = "autopep8", filetypes = { "python" } },
