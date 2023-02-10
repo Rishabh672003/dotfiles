842558ae84d3bdb9055850e12a8ff153c31df0b5
Rishabh <jharishabh2k3@gmail.com>
2023-02-10 11:59:25 +0530

    update

--- .config/zsh/nvim-linux64/share/nvim/runtime/plugin/nvim.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command('Inspect', function(cmd)
-  if cmd.bang then
-    vim.pretty_print(vim.inspect_pos())
-  else
-    vim.show_pos()
-  end
-end, { desc = 'Inspect highlights and extmarks at the cursor', bang = true })
