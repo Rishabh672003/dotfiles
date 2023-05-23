2009fccd2506e1aa536de477ffed67324301b978
Rishabh <jharishabh2k3@gmail.com>
2023-05-23 18:38:17 +0530

    update

--- .config/zsh.old/aliases.zsh
+++ .config/zsh.old/aliases.zsh
@@ -1,43 +1,48 @@
-#my aliases
+alias c='clear'
+alias e='exit'
+alias :q='exit'
+alias :wq='exit'
+alias f='fastfetch'
 alias m='mocp'
+alias n='neofetch'
+alias p='paru'
+alias q='exit'
+alias y='yay'
+alias cd='z'
+alias cf='clear;fastfetch'
+alias cn='c && n'
+alias cz='nvim ~/.config/zsh/.zshrc'
+alias cat='bat -p'
+alias cdd='zi'
+alias cdp='cd ..'
 alias cpc='clear;pfetch'
+alias pf='pfetch'
+alias ta='tmux attach'
+alias td='tmux detach'
+alias cnp='c;n;p'
+alias cny='c;n;y'
+alias cdpp='cd ../../'
 alias py='python'
 alias cali='nvim ~/.config/zsh/aliases.zsh'
 alias nr='sudo systemctl restart NetworkManager'
 alias sp='speedtest'
-alias y='yay'
 alias yss='yay -S --noconfirm --needed'
 alias ys='yay -Syu --noconfirm --devel'
 alias pss='paru -Syu --noconfirm --devel'
-alias p='paru'
 alias tpg='topgrade'
-alias n='neofetch'
-alias f='fastfetch'
 alias one='onefetch'
-alias c='clear'
-alias cd='z'
-alias cdd='zi'
-alias cdp='cd ..'
-alias cdpp='cd ../../'
-alias cz='nvim ~/.config/zsh/.zshrc'
-alias e='exit'
-alias q='exit'
+alias rm='rm -v'
 alias vs='sudoedit'
 alias lg='lazygit'
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias nv='nvim'
+alias nbi='NVIM_APPNAME=nvim-basic-ide nvim'
 alias nvc='nvim --clean'
-alias nnvim='~/Applications/nvim.appimage'
 alias hx='helix'
 alias lv='lvim'
-alias cat='bat -p'
-alias cn='c && n'
-alias cnp='c;n;p'
-alias cny='c;n;y'
-alias cf='clear;fastfetch'
 alias shut='shutdown now'
 alias rest='reboot'
-alias log='gnome-session-quit'
+alias xo='xdg-open'
 alias rs='sudo systemctl start rate-mirrors'
 alias rss='systemctl status rate-mirrors'
 alias rsss='sh ~/projects/configs/mirrors-and-hosts/rate-mirrors-arch.sh'
@@ -47,17 +52,17 @@ alias sys='system-monitoring-center'
 alias shut='shutdown now'
 alias sus='systemctl suspend'
 alias ls='exa --color always --icons -a'
-alias ll='exa --color always --icons -1albh -s name --git --sort date'
-alias lll='exa --color always --icons -1albhT -L 2 -s name -I .git --git'
+alias ll='exa --color always --icons -1albh -s name --git --sort date --group-directories-first'
+alias lll='exa --color always --icons -1albhT -L 2 -s name -I .git --git --group-directories-first'
 alias ws='waydroid show-full-ui'
 alias wss='XDG_SESSION_TYPE=wayland waydroid show-full-ui'
 alias we='waydroid session stop'
 alias wr='sudo systemctl restart waydroid-container'
 alias wl='sudo waydroid log'
-alias grub-update=' sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias install-grub='sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB'
+alias update-grub=' sudo grub-mkconfig -o /boot/grub/grub.cfg'
 alias wtr='curl -4 https://wttr.in/virar'
-alias gtnv='curl --output-dir ~/Applications/ \
-    -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage'
+alias nnv='~/Applications/nvim-linux64/bin/nvim'
 alias ua-drop-caches='yay -Sc --aur --noconfirm'
 alias ua-update-all='export TMPFILE="$(mktemp)"; \
     sudo true; \
@@ -67,14 +72,16 @@ alias ua-update-all='export TMPFILE="$(mktemp)"; \
     ua-drop-caches && \
     yay -Syyu --noconfirm'
 alias ngit='cp -r /home/rishabh/.config/nvim/* ~/projects/Neovim/ && \
-    cp ~/projects/init.lua ~/projects/Neovim/init.lua && \
     cd ~/projects/Neovim'
 alias zshc='cp ~/.config/zsh/* ~/projects/zsh-configs/ ; \
-    cd ~/projects/zsh-configs'
+    cd ~/projects/zsh-configs
+# git add * && \
+    # sh ~/git-repos/gc.sh && \
+    # git push'
 alias pgit='cp ~/python-study/* ~/projects/python-studies/ ; cd ~/projects/python-studies/'
 alias sdr='sudo docker run -it archlinux:base-devel'
 alias sdp='sudo docker system prune -a'
-alias uneo='sudo true && cd /home/rishabh/projects/random-repos/neovim/ && git pull && sudo make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install'
+alias uneo='bob install nightly'
 alias ngt='git clone https://github.com/rishabh672003/neovim ~/.config/nvim'
 alias hyp='cp -r ~/.config/hypr ~/projects/hyprland-configs/ \
     cp -r ~/.config/waybar ~/projects/hyprland-configs/ \
@@ -82,3 +89,31 @@ alias hyp='cp -r ~/.config/hypr ~/projects/hyprland-configs/ \
 alias nrandom='tr -dc "A-Za-z 0-9" < /dev/urandom | fold -w100 | head -n 1000000 > bigfile.txt'
 alias yl='sh ~/.config/yadm/yadm.sh'
 alias yel='yadm enter lazygit'
+alias yt="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'"
+alias sep="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' \
+    | xargs -ro sudo pacman -S"
+alias srp="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' \
+    | xargs -ro sudo pacman -Rns"
+
+alias binrm="rm -rf *.out; rm -rf */*.out; rm -rf */*/*.out; rm -rf */*/*/*.out"
+
+export ssep (){
+    pacman -Slq | fzf --multi --preview 'cat <(pacman -Si {1}) <(pacman -Fl {1} | awk "{print \$2}")' | xargs -ro sudo pacman -S
+}
+alias ssep=ssep
+
+export gtnv(){
+    if command -v aria2c &> /dev/null; then
+        cd ~/Applications/ &&
+        rm -rf ~/Applications/nvim-linux64 ~/Applications/nvim-linux64.tar.gz > /dev/null 2>&1 ;
+        aria2c "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+        tar -xvf ~/Applications/nvim-linux64.tar.gz > /dev/null 2>&1
+    else
+        cd ~/Applications/ && \
+            rm -rf ~/Applications/nvim-linux64 ~/Applications/nvim-linux64.tar.gz > /dev/null 2>&1 ; \
+            curl --output-dir ~/Applications/ \
+            -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz && \
+            tar -xvf ~/Applications/nvim-linux64.tar.gz > /dev/null 2>&1
+    fi
+}
+alias gtnv=gtnv
--- .config/zsh/.zprofile
+++ .config/zsh/.zprofile
@@ -1,6 +1,7 @@
 export EDITOR="nvim"
 export BROWSER="firefox"
 export VIDEO="mpv"
+export DIFFPROG="nvim -d"
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_CACHE_HOME=$HOME/.cache
@@ -16,7 +17,6 @@ export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export GOPATH="$XDG_DATA_HOME"/go
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
-export LESSHISTFILE="$XDG_CACHE_HOME"/less/history
 export TERMINFO="$XDG_DATA_HOME"/terminfo
 export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
 export HISTFILE="${XDG_CONFIG_HOME}"/zsh/history
@@ -31,5 +31,6 @@ export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 export XCURSOR_PATH=/usr/share/icons:${XDG_DATA_HOME}/icons
 export PARALLEL_HOME="$XDG_CONFIG_HOME"/parallel
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export LESSHISTFILE="$XDG_CACHE_HOME"/less/history
 export LESSOPEN='|~/.config/zsh/lessfilter.sh %s'
 export LESS=' -R '
