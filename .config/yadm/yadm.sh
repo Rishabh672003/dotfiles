7053a0f78f470b67c6341063e7e938c35ebe0115
Rishabh <jharishabh2k3@gmail.com>
2023-06-05 11:30:16 +0530

    update

--- .config/yadm/yadm.sh
+++ .config/yadm/yadm.sh
@@ -1,30 +1,29 @@
 #!/bin/env bash
 
 yadm add ~/README.md \
-    ~/.config/alacritty\
-    ~/.config/btop\
-    ~/.config/fish\
-    ~/.config/contour\
-    ~/.config/helix\
-    ~/.config/hypr\
-    ~/.config/kitty\
-    ~/.config/lvim\
-    ~/.config/mpv\
-    ~/.config/neofetch\
-    ~/.config/nvim\
-    ~/.config/rofi\
-    ~/.config/shellfetch\
-    ~/.config/sway\
-    ~/.config/tmux\
-    ~/.config/vim\
-    ~/.config/waybar\
-    ~/.config/wezterm\
-    ~/.config/wofi\
-    ~/.config/zsh\
-    ~/.config/wpaperd\
-    ~/.config/yadm\
-    ~/.config/spicetify\
+    ~/.config/alacritty \
+    ~/.config/btop \
+    ~/.config/fish \
+    ~/.config/contour \
+    ~/.config/helix \
+    ~/.config/hypr \
+    ~/.config/kitty \
+    ~/.config/lvim \
+    ~/.config/mpv \
+    ~/.config/neofetch \
+    ~/.config/nvim \
+    ~/.config/rofi \
+    ~/.config/shellfetch \
+    ~/.config/sway \
+    ~/.config/tmux \
+    ~/.config/vim \
+    ~/.config/waybar \
+    ~/.config/wezterm \
+    ~/.config/wofi \
+    ~/.config/zsh \
+    ~/.config/wpaperd \
+    ~/.config/yadm \
+    ~/.config/spicetify \
     ~/other-stuff
 
-yadm commit -am "update";
-yadm push
+yadm commit -am "update" && yadm push
