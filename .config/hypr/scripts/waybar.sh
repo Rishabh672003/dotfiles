8bd3d89e62d1937e4d16412bed34120ecfc0eced
Rishabh <jharishabh2k3@gmail.com>
2023-06-29 16:54:11 +0530

    update

--- .config/hypr/scripts/waybar.sh
+++ .config/hypr/scripts/waybar.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/env bash
 
-CONFIG_FILES1="$HOME/.config/waybar/config.jsonc"
-CONFIG_FILES2="$HOME/.config/waybar/style.css"
+export pid
+pid=$(pgrep waybar)
+status=$?
 
-trap "killall waybar" EXIT
-
-while true; do
-    waybar &
-    inotifywait -e modify "$CONFIG_FILES1"
-    inotifywait -e create,modify "$CONFIG_FILES2"
-    killall waybar
-done
+if [ $status != 0 ]; then
+    exec waybar;
+else
+    killall -9 waybar && waybar
+fi
