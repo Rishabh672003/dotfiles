69a1585a1865c2d43762bdfc85d3d69750c824c3
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-10-28 20:29:05 +0530

    update

--- .config/hypr/scripts/switch_kb_layout.sh
+++ .config/hypr/scripts/switch_kb_layout.sh
@@ -2,13 +2,13 @@
 
 layout_f=/tmp/kb_layout
 
-case $(cat "$layout_f") in 
-  us)
-    hyprctl keyword input:kb_layout il
-    echo il > $layout_f
-    ;;
-  il)
-    hyprctl keyword input:kb_layout us
-    echo us > $layout_f
-    ;;
+case $(cat "$layout_f") in
+us)
+	hyprctl keyword input:kb_layout il
+	echo il >$layout_f
+	;;
+il)
+	hyprctl keyword input:kb_layout us
+	echo us >$layout_f
+	;;
 esac
