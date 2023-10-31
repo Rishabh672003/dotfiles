7a58657e10d6f376b5703685e4edba9d67481fb7
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-10-31 23:11:47 +0530

    update

--- .config/yadm/yadm.sh
+++ .config/yadm/yadm.sh
@@ -1,30 +1,29 @@
 #!/bin/env bash
 
 yadm add ~/README.md \
-    ~/.config/alacritty \
-    ~/.config/btop \
-    ~/.config/dunst \
-    ~/.config/fish \
-    ~/.config/contour \
-    ~/.config/helix \
-    ~/.config/hypr \
-    ~/.config/kitty \
-    ~/.config/lvim \
-    ~/.config/mpv \
-    ~/.config/neofetch \
-    ~/.config/nvim \
-    ~/.config/rofi \
-    ~/.config/shellfetch \
-    ~/.config/sway \
-    ~/.config/tmux \
-    ~/.config/vim \
-    ~/.config/waybar \
-    ~/.config/wezterm \
-    ~/.config/wofi \
-    ~/.config/zsh \
-    ~/.config/wpaperd \
-    ~/.config/yadm \
-    ~/.config/spicetify \
-    ~/other-stuff
+	~/.config/alacritty \
+	~/.config/btop \
+	~/.config/dunst \
+	~/.config/fish \
+	~/.config/contour \
+	~/.config/helix \
+	~/.config/hypr \
+	~/.config/kitty \
+	~/.config/lvim \
+	~/.config/mpv \
+	~/.config/neofetch \
+	~/.config/rofi \
+	~/.config/shellfetch \
+	~/.config/sway \
+	~/.config/tmux \
+	~/.config/vim \
+	~/.config/waybar \
+	~/.config/wezterm \
+	~/.config/wofi \
+	~/.config/zsh \
+	~/.config/wpaperd \
+	~/.config/yadm \
+	~/.config/spicetify \
+	~/other-stuff # ~/.config/nvim \
 
 yadm commit -a -m "update" && yadm push
